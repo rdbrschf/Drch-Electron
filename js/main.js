aab363832160a2d0e46fd22cd64c1b4ae8e53d8f
HengiFettlich <hengifett@gmail.com>
2019-10-16 17:50:13 +0200

    bump, revert removal of escapeHTML

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+var entityMap = {
+	'&': '&amp;',
+	'<': '&lt;',
+	'>': '&gt;',
+	'"': '&quot;',
+	"'": '&#39;',
+	'/': '&#x2F;',
+	'`': '&#x60;',
+	'=': '&#x3D;'
+  };
+  
+  function escapeHtml(string) {
+	return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+	  return entityMap[s];
+	});
+  }
+  
 var $osd = $('#osd');
 var $viewers = $('.item.eye');
 var streamerID = "Drache_Offiziell";
